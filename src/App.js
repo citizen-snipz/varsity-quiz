915c1ac4d4153b06affc5ad66615daeb95acf035
Shara Crosslin <sharacrosslin@gmail.com>
2021-06-01 11:44:05 -0700

    added form component, changed question card format

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import "./App.css"
 import Header from "./components/Header"
 import QuestionCard from "./components/QuestionCard"
 import Footer from "./components/Footer"
-import questions from "./questionsDB"
 import React from "react"
+import questions from "./questionsDB"
+import AddQuestionForm from "./components/AddQuestionForm"
 
 class App extends React.Component {
-  constructor() {
-    super()
-  }
   render() {
     // const questionList = questionsDB.map(question => <QuestionCard key={question.id} />)
     return (
       <div className="App">
         <Header />
-        <QuestionCard />
+        {/* {questions.map((question) => (
+          <QuestionCard key={question.id} {...question} />
+        ))} */}
+        <AddQuestionForm />
         <Footer />
       </div>
     )
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,17 +1,15 @@
 import React from "react"
-import questions from "../questionsDB"
 
 function QuestionCard(props) {
   //Make color-coded tags display at bottom of card
-  const tags = questions.map((question) => (
-    <QuestionCard key={question.id} categories={question.categories} />
-  ))
   return (
     <div>
       <h3>Question: {props.question}</h3>
       <h3>Answer: {props.answer}</h3>
-      <h3>{props.bonus && `"Bonus Question: " ${props.bonus}`}</h3>
-      <h4>Tags: {tags}</h4>
+      <h3>{props.bonus && `Bonus Question:  ${props.bonus}`}</h3>
+      {/* todo: Make color-coded tags display at bottom of card */}
+
+      <h4>Tags: {props.categories}</h4>
     </div>
   )
 }
