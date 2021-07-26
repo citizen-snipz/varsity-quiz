dfd73b7d1568f749074755c669b8d412c611c989
Shara Crosslin <sharacrosslin@gmail.com>
2021-07-25 19:59:41 -0700

    style tags on question card

--- src/components/FilterOptions.js
+++ src/components/FilterOptions.js
@@ -2,72 +2,91 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 function FilterOptions(props) {
-  console.log("FilterOptions props: ", props)
-  const categoryInitializer = props.dataOptions.categories.reduce(
-    (obj, cat) => {
-      const propName = cat.toLowerCase()
-      obj[propName] = false
-      return obj
-    },
-    {}
-  )
-  const [categoryState, setCategoryState] = useState(categoryInitializer)
+  const { dataOptions, requestApi } = props
+
+  const categoryInitializer = dataOptions.categories.reduce((obj, cat) => {
+    const propName = cat.toLowerCase()
+    obj[propName] = false
+    return obj
+  }, {})
+
+  const [formState, setFormState] = useState({
+    categories: categoryInitializer,
+    year: "",
+    match: ""
+  })
 
   function handleCategoryClick(event) {
     const categoryButtonName = event.target.defaultValue.toLowerCase()
-    setCategoryState((prevState) => {
+    setFormState((prevState) => {
       return {
         ...prevState,
-        [categoryButtonName]: !prevState[categoryButtonName]
+        categories: {
+          ...prevState.categories,
+          [categoryButtonName]: !prevState[categoryButtonName]
+        }
       }
     })
   }
 
-  const categoryInputs = props.dataOptions.categories.map((category, i) => (
+  const categoryInputs = dataOptions.categories.map((category, i) => (
     <input
       key={i}
       className={`filterCategoryBtn ${
-        categoryState[category.toLowerCase()] && "selected"
+        formState.categories[category.toLowerCase()] && "selected"
       }`}
       type="button"
       value={category}
       onClick={handleCategoryClick}
     />
   ))
-  const yearOptions = props.dataOptions.year.map((availYear, i) => (
+  const yearOptions = dataOptions.year.map((availYear, i) => (
     <option key={i} value={availYear}>
       {availYear}
     </option>
   ))
-  const matchOptions = props.dataOptions.match.map((availMatch, i) => (
+  const matchOptions = dataOptions.match.map((availMatch, i) => (
     <option key={i} value={availMatch}>
       {availMatch}
     </option>
   ))
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    requestApi("post", "/questions/filter", formState)
+  }
+
+  function handleChange(event) {
+    console.log(event.target.value)
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
   return (
     <React.Fragment>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Search for Questions:</h2>
         <p>Filter by category: {categoryInputs}</p>
         <label>
           Filter by year:{" "}
-          <select name="year">
+          <select name="year" onChange={handleChange}>
             <option value="">Year</option>
             {yearOptions}
           </select>
         </label>
         <label>
           Filter by match:{" "}
-          <select name="match">
+          <select name="match" onChange={handleChange}>
             <option value="">Match</option>
             {matchOptions}
           </select>
         </label>
+        <button>Get Practice Questions</button>
       </form>
-      <Link to="/feed" className="practiceBtn">
-        Get Practice Questions!
-      </Link>
     </React.Fragment>
   )
 }
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -4,11 +4,20 @@ function QuestionCard(props) {
   return (
     <div className="card">
       <h3>Question: {props.question}</h3>
-      <h3>Answer: {props.answer}</h3>
+      <h3 className="answer">Answer: {props.answer}</h3>
       <h3>{props.bonus && `Bonus Question:  ${props.bonus}`}</h3>
       {/* todo: Make color-coded tags display at bottom of card */}
 
-      <h4>Tags: {props.categories}</h4>
+      <h4>Tags:</h4>
+      <ul className="tagsList">
+        {props.categories.map((tag) => {
+          return (
+            <li key={tag} className={tag}>
+              {tag}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
